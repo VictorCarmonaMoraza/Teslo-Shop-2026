9388463ba0d858d019ae4d401db8058166aed8ee
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-25 11:32:04 +0100

    Pagina de productos por genero

--- src/app/store-front/pages/gender-page/gender-page.ts
+++ src/app/store-front/pages/gender-page/gender-page.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { ProductCard } from "@/products/components/product-card/product-card";
+import { ProductsService } from '@/products/services/products.service';
+import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-gender-page',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './gender-page.html',
   styleUrl: './gender-page.css',
 })
 export class GenderPage {
+
+  route = inject(ActivatedRoute);
+  private productsService = inject(ProductsService);
+
+  gender = toSignal(this.route.params.pipe(
+    map(({ gender }) => gender)
+  ))
+
+  productsResource = rxResource({
+    params: () => ({ gender: this.gender() }),
+    stream: ({ params }) => {
+      return this.productsService.getProducts({ gender: params.gender });
+    }
+  });
 
 }
