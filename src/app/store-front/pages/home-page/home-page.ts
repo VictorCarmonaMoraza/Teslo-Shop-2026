5162f806a18b069362a9384e26578d85fd3a3ba9
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-25 13:12:17 +0100

    Paginacion local

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -4,11 +4,12 @@ import { ProductsService } from '@/products/services/products.service';
 import { Component, inject } from '@angular/core';
 // import { ProductCard } from "../../../products/components/product-card/product-card";
 import { rxResource } from '@angular/core/rxjs-interop';
+import { Pagination } from "@/shared/components/pagination/pagination";
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductCard],
+  imports: [ProductCard, Pagination],
   templateUrl: './home-page.html',
   styleUrl: './home-page.css',
 })
