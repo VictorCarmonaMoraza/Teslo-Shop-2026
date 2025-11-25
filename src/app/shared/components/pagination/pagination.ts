039adbd4e181893028551f782dc57b56b1010d8d
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-25 13:38:59 +0100

    Paginacion local  - Funcionalidad

--- src/app/shared/components/pagination/pagination.ts
+++ src/app/shared/components/pagination/pagination.ts
@@ -1,18 +1,21 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, linkedSignal } from '@angular/core';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-pagination',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './pagination.html',
   styleUrl: './pagination.css',
 })
 export class Pagination {
-
-
+  //Numero de paginas que se las pasa desde el componente padre
   pages = input(0);
+  //Pagina actual en la que estamos
   currentPage = input<number>(1);
+  // linkedSignal sincroniza activePage con currentPage (dos direcciones)
+  activePage = linkedSignal(this.currentPage);
 
-  //
+  // Genera un array [1, 2, 3, ..., pages]
   getPagesList = computed(() => {
     return Array.from({ length: this.pages() }, (_, i) => i + 1);
   });
