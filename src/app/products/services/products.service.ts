60172c2e47e26a954664ca8c0b463d464eae8675
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-26 10:43:03 +0100

    Product cache

--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Observable, of, tap } from 'rxjs';
+import { delay, Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Product, ProductsResponse } from '../interfaces/product.interface';
 
@@ -18,6 +18,8 @@ export class ProductsService {
 
   private http = inject(HttpClient);
   private productsCache = new Map<string, ProductsResponse>();
+
+  private productCache = new Map<string, Product>();
 
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
@@ -43,13 +45,18 @@ export class ProductsService {
       );
   }
 
-
-
   //Metodo para obtener producto por slug
   getProductBySlug(idSlug: string): Observable<Product> {
+
+    if (this.productCache.has(idSlug)) {
+      return of(this.productCache.get(idSlug)!);
+    }
+
     return this.http.get<Product>(`${baseUrl}/products/${idSlug}`)
       .pipe(
-        tap(resp => console.log('getProductBySlug', resp))
+        delay(2000),
+        tap(resp => console.log('getProductBySlug', resp)),
+        tap(resp => this.productCache.set(idSlug, resp))
       );
   }
 
