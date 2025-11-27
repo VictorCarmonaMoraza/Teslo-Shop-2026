4b811df9fc5c1eee52724764c5bdc33d2f545184
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-27 19:50:54 +0100

    Diseño del panel administrativo

--- src/app/admin-dashboard/layout/admin-dashboard-layout/admin-dashboard-layout.ts
+++ src/app/admin-dashboard/layout/admin-dashboard-layout/admin-dashboard-layout.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@/auth/services/auth.service';
+import { Component, computed, inject } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './admin-dashboard-layout.html',
   styleUrl: './admin-dashboard-layout.css',
 })
 export class AdminDashboardLayout {
 
+  authService = inject(AuthService);
+
+  //señal computada para obtener el nombre de usuario
+  user = computed(() => this.authService.user());
 }
