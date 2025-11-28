c8e967c97722fe996a7ba8fab9c973a2d6244ff8
Victor Carmona <VictorCarmona9035@hotmail.com>
2025-11-28 13:58:57 +0100

    Formulario de producto

--- src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -1,16 +1,53 @@
 import { ProductCarousel } from '@/products/components/product-carousel/product-carousel';
 import { Product } from '@/products/interfaces/product.interface';
-import { Component, input } from '@angular/core';
+import { FormUtils } from '@/utils/form-utils';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-product-details',
-  imports: [ProductCarousel],
+  imports: [ProductCarousel, ReactiveFormsModule],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css',
 })
-export class ProductDetails {
+export class ProductDetails implements OnInit {
+
   //Para mostrar el producto que le pasamos por el input
   product = input.required<Product>();
 
+  //Inyecciones
+  fb = inject(FormBuilder);
+
+  //Variables
   sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+
+  //Creamos un formulario reactivo
+  productForm = this.fb.group({
+    title: ['', [Validators.required]],
+    description: ['', [Validators.required]],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    images: [[]],
+    tags: [''],
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]]
+  })
+
+  ngOnInit(): void {
+    this.setFormValue(this.product())
+  }
+
+  setFormValue(formLike: Partial<Product>) {
+    //Hace lo mismo que la siguiente linea
+    //this.productForm.patchValue(formLike as any);
+    this.productForm.reset(this.product() as any)
+    this.productForm.patchValue({ tags: formLike.tags?.join(',') })
+  }
+
+  onSubmit() {
+    //Mostramos datos del formulario
+    console.table(this.productForm.value)
+  }
+
 }
